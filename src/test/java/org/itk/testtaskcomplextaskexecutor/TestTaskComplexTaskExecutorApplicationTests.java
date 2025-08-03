828c80470428584c7a7faa01d61ae19f5cbbe293
Canib-us <balev.danilll@gmail.com>
2025-08-03 17:36:45 +0700

    Initial commit

---
+++ src/test/java/org/itk/testtaskcomplextaskexecutor/TestTaskComplexTaskExecutorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.itk.testtaskcomplextaskexecutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskComplexTaskExecutorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
